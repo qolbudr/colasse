8faccbe47d0b1e99bb3ee9ad625b5b08079617b1
qolbudr <qolbu.dzikru30@gmail.com>
2021-04-04 00:27:41 -0700

    Fix Garek Copas

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$('.word-carousel').owlCarousel({
+    nav: true,
+    dots: false,
+    navText: ["<a class=\"btn-circle\"><img src=\"img/icon/left.svg\" height=\"15\"></a>", "<a class=\"btn-circle\"><img src=\"img/icon/right.svg\" height=\"15\"></a>"],
+    responsive:{
+        0:{
+            items:1
+        },
+    }
+})
+$('.menu-mobile').click(function() {
+  $('.navigation-mobile').css('visibility', 'visible');
+  $('.navigation-mobile').css('opacity', '100%');
+})
+$('.menu-close').click(function() {
+  $('.navigation-mobile').css('visibility', 'hidden');
+  $('.navigation-mobile').css('opacity', '0');
+})
