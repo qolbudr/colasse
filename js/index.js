3b029bd340d4fa6ba229b11633ec5b46af06c2ca
qolbudr <qolbu.dzikru30@gmail.com>
2021-04-04 03:12:39 -0700

    Selesai

--- js/index.js
+++ js/index.js
@@ -23,12 +23,51 @@ $('.menu-close').click(function() {
 })
 
 $('.blog-carousel').owlCarousel({
-        nav: true,
-        dots: false,
-        navText: ["<a class=\"btn-circle\"><img src=\"img/icon/left.svg\" height=\"15\"></a>", "<a class=\"btn-circle\"><img src=\"img/icon/right.svg\" height=\"15\"></a>"],
-        responsive:{
-            0:{
-                items:1
-            },
-        }
-    })
+    nav: true,
+    dots: false,
+    navText: ["<a class=\"btn-circle\"><img src=\"img/icon/left.svg\" height=\"15\"></a>", "<a class=\"btn-circle\"><img src=\"img/icon/right.svg\" height=\"15\"></a>"],
+    responsive:{
+        0:{
+            items:1
+        },
+    }
+})
+
+$(".btn-slide").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#summary").offset().top
+    }, 1000);
+});
+
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('.info-covid').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.info-covid .font-medium').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
